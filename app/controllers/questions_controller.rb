f89d005b66c444c65a5083521481775b1cae8be6
paola pesce <paojpesce@gmail.com>
2022-09-08 21:55:48 -0300

    final ejercicio 2

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,7 +1,17 @@
 class QuestionsController < ApplicationController
 
   def ask
-    @question = questions
+
   end
 
+  def answer
+    @user_answer = params[:question]
+    if @user_answer == 'I am going to work!'
+      @coach_answer = 'Great!'
+    elsif @user_answer.end_with?('?')
+      @coach_answer = 'Silly question, get dressed and go to work!'
+    else
+      @coach_answer = 'I don t care, get dressed and go to work!'
+    end
+  end
 end
